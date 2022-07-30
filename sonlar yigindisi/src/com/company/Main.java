d3bb33f9dca03847d94f04b189408fc27d6f536b
shermuhammad2007 <110012651+shermuhammad2007@users.noreply.github.com>
2022-07-30 12:15:27 +0500

    Add files via upload

---
+++ sonlar yigindisi/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 10;
+        int sum = 0;
+
+        for(int i=0; i<=a; i++){
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+    }
+}
